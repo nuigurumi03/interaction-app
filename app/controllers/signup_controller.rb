519b90492b9b8306bbcdddfc80efda0ec905bf93
nuigurumi03 <nuigurumi03@gmail.com>
2020-01-21 13:00:52 +0900

    不要な記述の削除

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -135,64 +135,13 @@ class SignupController < ApplicationController
       reset_session
       render :address
     end
-end
-    # check_profile_valid = @profile.valid?
-    # unless check_user_valid && check_profile_valid
-    #   render 'signup/address'
-    # else
-    #   session[:through_second_validation] = "through_second_validation"
-    #   redirect_to action:create
-    # end
-  # end
-
-  # def create
-  #   @user = User.new(nickname: session[:nickname],email: session[:email],password: session[:password],password_confirmation: session[:password_confirmation])
-  #   # 万一ユーザーがcreateできなかった場合、全sessionをリセットして登録ページトップへリダイレクト
-  #   unless @user.save
-  #     reset_session
-  #     redirect_to signup_index_path
-  #     return
-  #   end
-  #   # userが作れたらuserに紐づけてprofileを作ります
-  #   @profile = Profile.create(
-  #     user: @user,
-  #     birthyear: session[:birthyear],
-  #     birthmonth: session[:birthmonth],
-  #     birthday: session[:birthday],
-  #     family_name: session[:family_name],
-  #     personal_name: session[:personal_name],
-  #     family_name_kana: session[:family_name_kana],
-  #     personal_name_kana: session[:personal_name_kana],
-  #     prefecture: session[:prefecture],
-  #     city: session[:city],
-  #     postal_code: session[:postal_code],
-  #     tel: session[:tel],
-  #     house_code: session[:house_code],
-  #     bulid_name: session[:bulid_name]
-  #   )
-  #   # 最後のフォームでクレジット認証を行なっているため、ここでカードの顧客情報を作り、userと紐づけてDBに保存する処理を行なっています
-  #   # customer = Payjp::Customer.create(card: params[:payjp_token])
-  #   # @creditcard = Creditcard.new(user: @user,customer_id: customer.id,card_id: customer.default_card)
-  #   # カード情報まで保存に成功したら全sessionをリセットしてユーザーidのみsessionに預け、完了画面へリダイレクト
-  #   if @user.save
-  #     reset_session
-  #     session[:id] = @user.id
-  #     redirect_to done_signup_index_path
-  #     return
-  #   else
-  #     #失敗したらsessionを切って登録ページトップへリダイレクト
-  #     reset_session
-  #     redirect_to signup_index_path
-  #   end
-  # end
+  end
 
   def done
-    # session[id]がなければ登録ページトップへリダイレクト
     unless session[:id]
       redirect_to signup_index_path 
       return
     end
-    # deviseのメソッドを使ってログイン
     sign_in User.find(session[:id])
   end
 
