97d5ac4764afb1ee50893db2bc65d4c3ff9db030
nuigurumi03 <nuigurumi03@gmail.com>
2020-01-29 15:38:39 +0900

    d

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,8 @@
 class MessagesController < ApplicationController
-  before_action :set_post
+  # before_action :set_post
 
   def new
+    @post = Post.find(params[:post_id])
     @message = Message.new
   end
   
@@ -20,7 +21,7 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
   end
 
-  def set_post
-    @post = Post.find(params[:post_id])
-  end
+  # def set_post
+  #   @post = Post.find(params[:post_id])
+  # end
 end
